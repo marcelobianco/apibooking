a2a15796571779066933bf83b364020df0256c61
unknown <marcelognn@gmail.com>
2024-03-29 13:06:32 -0300

    Criado registro de usuário

---
+++ app/Http/Controllers/Api/V1/Auth/RegisterController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Enums\Roles;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Password::defaults()],
+            'role_id' => ['required', Rule::in(Roles::ROLE_OWNER, Roles::ROLE_USER)],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $user->assignRole(intval($request->role_id));
+
+        return response()->json([
+            'access_token' => $user->createToken('client')->plainTextToken,
+        ], 201);
+    }
+}
